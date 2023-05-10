2b3f5391d4addfb5a0484ccf2792cedca0cc4b3e
LaXnZ <sumudithalanz@gmail.com>
2023-05-10 17:33:56 +0530

    Error handling using express middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/errorMiddleware");
 const userRoute = require("./routes/users/usersRoute");
 
 const app = express();
@@ -12,5 +16,9 @@ app.use(express.json());
 
 //routes
 app.use("/", userRoute);
+
+//errorHandlers
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
--- src/controllers/users/usersController.js
+++ src/controllers/users/usersController.js
@@ -1,21 +1,19 @@
+const expressAsyncHandler = require("express-async-handler");
 const User = require("../../model/User");
 
 //Register
-
-const registerUser = async (req, res) => {
+const registerUser = expressAsyncHandler(async (req, res) => {
   const { email, firstname, lastname, password } = req?.body;
 
+  //check if the user exist
+  const userExist = await User.findOne({ email });
+  if (userExist) throw new Error("User already exists");
   try {
-    //check if the user exist
-    const userExist = await User.findOne({ email });
-    if (userExist) {
-      res.json("User Exist");
-    }
     const user = await User.create({ email, firstname, lastname, password });
     res.status(200).json(user);
   } catch (error) {
     res.json(`Error : ${error}`);
   }
-}; 
+});
 
 module.exports = { registerUser };
